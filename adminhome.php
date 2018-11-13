47a8c37f5cc24ae13a7c3d2704775e68a377971a
ADARSH A <adarsh3heartz@gmail.com>
2018-11-14 01:02:31 +0530

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -7,19 +7,90 @@ error_reporting(1);
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Untitled Document</title>
-S<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
+<style>
+* {
+    box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+    background-color: #f1f1f1;
+    padding: 20px;
+    text-align: center;
+}
+
+/* Style the top navigation bar */
+.topnav {
+    overflow: hidden;
+    background-color: #333;
+    height: 80px;
+    border: 25px;
+}
+
+/* Style the topnav links */
+.topnav a {
+    float: left;
+    display: block;
+    color:  #f2f2f2;
+    text-align: center;
+    font-size: 20px;
+    font-family: verdana;
+    font-style: bold;
+    padding: 30px 30px;
+    text-decoration: none;
+}
+
+/* Change color on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+.column {
+    float: left;
+    width: 100%;
+    padding: 10px;
+    height: 100%;
+
+   
+}
+.column1 {
+    float: left;
+    width: 25%;
+    padding-left: 10px;
+    padding-top: 23px;
+    padding-bottom: 23px;
+    
+   
+}
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+    padding: 10px
+}
+</style>
 
 </head>
 
 <body>
+	<div class="topnav">
+  <a href="index.php">HOME</a>
+  <a href="index.php?chk=about">ABOUT US</a>
+  <a href="index.php?chk=registraion">NEW USER</a>
+  <a href="index.php?chk=contact">CONTACT US</a>
+  <a href="index.php?chk=admin">ADMIN</a>
+</div >
+<div class="row" align="center" style="background-image: linear-gradient(to bottom right, #cfd9df, #e2ebf0);">
+   <div class="column" >
 
-<table width="1430"  style="background-image: url(newimage/ad.jpg);"   align="center" >
+<table width="100"  style="background-image: url(newimage/ad.jpg);"   align="center" >
   <tr >
-    <td height="230"  width="1430"   style="background-image:url(newimage/admin.jpg)" >
-	
-	
-	
-		</td>
+    
   </tr>
   <tr >
     <td height="100"  width="1430"  valign="top" colspan="3"  style="background-image:url(newimage/kk.png)" >
@@ -83,8 +154,12 @@ if($chk=="pass")
 	  echo"</td>";
 	}
 ?>
+</div>
+   <div class="column1">
 </tr>
   </table>
+  	</div>
+</div>
 
 
 
