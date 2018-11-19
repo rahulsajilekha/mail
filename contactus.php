5258e6619104cc7a85c63ed4954ec373ec943af7
Adarsh534 <adarsh3heartz@gmail.com>
2018-11-19 09:16:59 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -11,37 +11,31 @@ error_reporting(1);
 .card {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     max-width: 300px;
+    height: 400px;
     margin: auto;
     text-align: center;
-}
-
-.title {
-    color: grey;
-    font-size: 18px;
-}
-
-button {
-    border: none;
-    outline: 0;
-    display: inline-block;
-    padding: 8px;
-    color: white;
-    background-color: #000;
-    text-align: center;
-    cursor: pointer;
-    width: 100%;
-    font-size: 18px;
-}
-
-a {
-    text-decoration: none;
-    font-size: 22px;
     color: black;
+   
+
+   
+    
+
+}
+.title {
+    color: black;
+    font-size: 18px;
 }
 
-button:hover, a:hover {
-    opacity: 0.7;
-}
+.pa {
+   
+     background: white;
+   padding: 100px;
+   padding-left: 50px;
+   padding-right: 50px;
+   height: 100%;
+   box-shadow: 0px 2px 4px grey;
+   border-radius: 10px;
+}    
 </style>
     
 </head>
@@ -49,38 +43,52 @@ button:hover, a:hover {
 <body>
   
 
-<div class="card">
-  <img src="sundar.jpg" alt="John" style="width:100%">
+<div class=pa > 
+  <table>
+    <tr>
+      <td>
+<div class="card" >
+  <img src="sundar.jpg" alt="John" style="width:50%">
   <h1>Sundar Pichai</h1>
   <p class="title">CEO & Founder</p>
   <p>Harvard University</p>
-  <a href="#"><i class="fa fa-dribbble"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
-  <a href="#"><i class="fa fa-facebook"></i></a> 
-  <p><button>Contact</button></p>
+  <p>Email : mailserver@gmail.com</p> 
 </div>
-<div class="card">
-  <img src="markzuc.jpg" alt="Mark" style="width:100%">
+</td>
+<td>
+</td>
+<td>
+  </td>
+  <td>
+  </td>
+<td>
+<div class="card" >
+  <img src="markzuc.jpg" alt="Mark" style="width:70%">
   <h1>Mark Zuck</h1>
   <p class="title">Manager</p>
   <p>Oxford University</p>
-  <a href="#"><i class="fa fa-dribbble"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
-  <a href="#"><i class="fa fa-facebook"></i></a> 
-  <p><button>Contact</button></p>
-</div>
-<div class="card">
-  <img src="Stevejo.jpg" alt="Steve" style="width:100%">
+  <p>Email : mailservermanager@gmail.com</p>
+  <p>MOB : 8887776664</p>
+ </div>
+</td>
+<td>
+</td>
+<td>
+  </td>
+  <td>
+  </td>
+<td>
+<div class="card" >
+  <img src="Stevejo.jpg" alt="Steve" style="width:40%">
   <h1>Steve Jo</h1>
   <p class="title">Assistant Manager</p>
   <p>Kerala University</p>
-  <a href="#"><i class="fa fa-dribbble"></i></a> 
-  <a href="#"><i class="fa fa-twitter"></i></a> 
-  <a href="#"><i class="fa fa-linkedin"></i></a> 
-  <a href="#"><i class="fa fa-facebook"></i></a> 
-  <p><button>Contact</button></p>
+  <p>Email : mailserverasstm@gmail.com</p>
+  <p>MOB : 8887776663</p>
+  </td>
+</div>
+</tr>
+</table>
 </div>
 
 </body>
