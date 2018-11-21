f481af040aae03c4d9cbc81e5fc387de3f3ad968
ADARSH A <adarsh3heartz@gmail.com>
2018-11-21 22:10:24 +0530

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -18,17 +18,27 @@
     .about
     {
     	padding-top: 5px;
-    	 background-color: #333;
+    	background-color: #333;
     	opacity: .5;
         height: 650px;
         border-radius: 10px;
-        box-shadow: 2px 2px 8px grey; 
+        box-shadow: 2px 2px 8px grey;
+        background-image: "office.jpg";
+      
        
 	}
-	p
-	{
+	.ab{
+		  column-count: 3; 
+		  padding: 30px;
+		  column-rule-style: solid black;
+
+		      /*column-rule-style: solid black;
+		      column-rule-width: 1px;
+		      column-rule-color: lightblue;*/
+	
 		color: white;
-		font-family: verdana;
+		font-family: Timesnewroman;
+		font-size: 20px;
 		text-shadow: 2px 1px black;
 	}
     </style>
@@ -41,19 +51,10 @@
 	<div class="about">
 
 	<h2>About iMAIL</h2>
-<p>About Phptpoint.Com
-Phptpoint’s free PHP Online Tutorial has heaps of PHP Interview question and well-run Interview question with answer associated to core php, cake php, codeigniter, mysql, joomla etc. which can assist you to crack the Interview for PHP developer.</p>
+	<div class="ab">
+Phptpoint’s free PHP Online Tutorial has heaps of PHP Interview question and well-run Interview question with answer associated to core php, cake php, codeigniter, mysql, joomla etc. which can assist you to crack the Interview for PHP developer.Here, at phptpoint we begin from the scratch where we can lead you on the practice of these languages. This is a free and online site where the students who wants to build a career in the same profession, can be trained about the working of the PHP. Perhaps you can go to any of the institutes where you have to spend a hefty amount also and apart from that you need to give time as well. But here at Phptpoint we provide functional and easy to understand PHP tutorials for beginners with examples. This is an advantage of the PHP tutorial for PHP interested students as well as the experts of PHP programming who can learn many more things through our site. There is always distrust in the wits of the beginners that a site can’t help them in developing a better mode of understanding. But your search for a good tutorial ends up at Phptpoint. We have made this site for the benefit of the people who approach us in the zest of learning, so we have developed it in a way which can sort out the queries hand to hand. The visitors visiting to Phptpoint can directly ask for a solution to their queries. Once we know your trouble point, our job begins to find an apt way out for you. Apart from free online tutorial Phptpoint provides you free downloading facility also. Students can easily download free php projects. There are huge numbers of projects available for your reference. You can select them to see as an example and start learning through those live examples, because examples are the best ways for describing a particular thing. You can pick any topic from the online php tutorial and we will guide you with a complete package from the definition to the explanation in a well versed manner. 
 
-<p>Here, at phptpoint we begin from the scratch where we can lead you on the practice of these languages. This is a free and online site where the students who wants to build a career in the same profession, can be trained about the working of the PHP. Perhaps you can go to any of the institutes where you have to spend a hefty amount also and apart from that you need to give time as well. But here at Phptpoint we provide functional and easy to understand PHP tutorials for beginners with examples. This is an advantage of the PHP tutorial for PHP interested students as well as the experts of PHP programming who can learn many more things through our site. </p>
-
-
-<p>
-There is always distrust in the wits of the beginners that a site can’t help them in developing a better mode of understanding. But your search for a good tutorial ends up at Phptpoint. We have made this site for the benefit of the people who approach us in the zest of learning, so we have developed it in a way which can sort out the queries hand to hand. The visitors visiting to Phptpoint can directly ask for a solution to their queries. Once we know your trouble point, our job begins to find an apt way out for you. 
-</p>
-
-<p>
-Apart from free online tutorial Phptpoint provides you free downloading facility also. Students can easily download free php projects. There are huge numbers of projects available for your reference. You can select them to see as an example and start learning through those live examples, because examples are the best ways for describing a particular thing. You can pick any topic from the online php tutorial and we will guide you with a complete package from the definition to the explanation in a well versed manner. 
-</p>
+</div>
 </div>
 </div>
 </body>
